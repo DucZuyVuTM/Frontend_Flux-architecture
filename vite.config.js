d129c0b7074067266416f6b8f555ec11a80cd1ee
VuDucZuy <duczuyvu12@gmail.com>
2025-04-14 17:52:33 +0300

    Working for Frontend_Flux-architecture

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/project_Frontend-Flux-Architecture/', // Thay bằng tên repository của bạn
 });
